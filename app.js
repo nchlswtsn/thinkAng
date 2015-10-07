649c5b256db997dcd36244b15a7f02c882c7e81d
Charles Watson <nchlswtsn@gmail.com>
2015-10-07 12:03:44 -0700

    basic angular message posting and reading service

---
+++ app.js
@@ -0,0 +1,31 @@
+angular.module('app', []);
+
+angular.module('app').factory('messages', function(){
+  var messages = {};
+
+  messages.list = [];
+
+  messages.add = function(message){
+    messages.list.push({id: messages.list.length, text: message});
+  };
+
+  return messages;
+});
+
+angular.module('app').controller('ListCtrl', function (messages){
+  var self = this;
+
+  self.message = messages.list;
+});
+
+angular.module('app').controller('PostCtrl', function(messages){
+  var self = this;
+
+  self.newMessage = 'Hello World!';
+
+  self.addMessage = function(message){
+    messages.add(message);
+    self.newMessage = '';
+
+  };
+});
